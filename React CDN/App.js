dd11004d861aafdafd8e13a47567ae9a7bb96ba9
asif-mohmd <asifasifpsps@gmail.com>
2024-01-04 20:42:12 +0530

    Comparison Between React.createElement & JSX

--- React CDN/App.js
+++ React CDN/App.js
@@ -6,10 +6,15 @@ const parent = React.createElement("div",{id: "parent"},
 React.createElement("div",{id: "child"},
 React.createElement("h1",{id: "parent"},"This is Hhhhh1 tag")))
 
+
 console.log(parent)
+
+const jsxHeading = <h1 id="heading">New Tag Test</h1>
+
+console.log(jsxHeading)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent)
+root.render(jsxHeading)
 
 
