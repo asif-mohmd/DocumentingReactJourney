a347213d4bd85914300d43de2056ecc94b031516
asif-mohmd <asifasifpsps@gmail.com>
2024-01-04 22:01:14 +0530

    Writing React Element inside a Component

--- React CDN/App.js
+++ React CDN/App.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client"
 
 // normal React Element
 
-const Heading2 = () => (
+const heading2 =  (
     <h1 className="head" tabIndex="5">
         Hello World
     </h1>
@@ -15,10 +15,10 @@ const number = 1000
 // React Functional Component
  const HeadingComponent = () =>(
     <div className="container">
-        <Heading2/>
-
         {number}
-        
+
+        {heading2}
+
         <h1 className="heading">Function Component in React</h1>
     </div>
  )
