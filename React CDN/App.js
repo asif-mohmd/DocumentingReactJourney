d97b07ff8e62535596a99845c2ade6fd49d1fd67
asif-mohmd <asifasifpsps@gmail.com>
2024-01-04 15:43:58 +0530

    created react app like a npx create app by using parcel with handsown

--- React CDN/App.js
+++ React CDN/App.js
@@ -1,7 +1,12 @@
 
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div",{id: "parent"},
 React.createElement("div",{id: "child"},
-React.createElement("h1",{id: "parent"},"This is H1 tag")))
+React.createElement("h1",{id: "parent"},"This is Hhhhh1 tag")))
+
+console.log(parent)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
